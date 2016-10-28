131196352c4111a92a9a1e88c7e4a595cf41e374
Neuro3en <bhp91@hotmail.com>
2016-10-28 13:14:01 +0200

    Add files via upload

---
+++ codeFeeder.ino
@@ -0,0 +1,288 @@
+// Feeding part of the artificial flower.
+// by Benjamin H Paffhausen; MIT Licence
+// Steppermotor controlled by analoginputs that measure if teh sucrosewater 
+// is present in certain parts of the feeding tube.
+// Most of this code is based on: https://github.com/tardate/X113647Stepper
+
+
+int Pin0 = 8;
+int Pin1 = 9;
+int Pin2 = 10;
+int Pin3 = 11;
+boolean forward = false;
+
+int max_steps = 65;
+int step = 0;
+  int belohnung = HIGH;   
+unsigned long previousMillis = 0;  
+unsigned long interval = 60000; 
+
+void setup() {
+      pinMode(4,INPUT);
+      pinMode(12,OUTPUT);
+      
+  Serial.begin(9600);
+
+  pinMode(Pin0, OUTPUT);
+  pinMode(Pin1, OUTPUT);
+  pinMode(Pin2, OUTPUT);
+  pinMode(Pin3, OUTPUT);
+ 
+  
+}
+
+void loop() {
+//  if (digitalRead(4)==HIGH){
+//  if (analogRead(A0)<400){
+ unsigned long currentMillis = millis();
+ if (currentMillis - previousMillis >= interval) {
+    // save the last time you blinked the LED
+    previousMillis = currentMillis;
+   // int belohnung=1;
+   if (belohnung == LOW) {
+      belohnung = HIGH;
+
+      interval = 20000;
+    } else {
+      belohnung = LOW;
+      interval = 380000;
+    }
+    }
+if (belohnung == HIGH){
+  // analogWrite(3, 3);  // LED on pin D3, brightness 3 out of 255
+   digitalWrite(3, HIGH);
+
+digitalWrite(12, HIGH);
+}else{
+   //  analogWrite(3, 0);  // LED on pin D3, OFF
+     digitalWrite(12, LOW);
+digitalWrite(3, LOW);
+
+}
+
+ 
+   
+int sensorValue = analogRead(A6);
+  int sensorValue2 = analogRead(A5);  
+
+if (sensorValue>400 && belohnung == HIGH){  // should be full and it is
+digitalWrite(Pin0, LOW);
+      digitalWrite(Pin1, LOW);
+      digitalWrite(Pin2, LOW);
+      digitalWrite(Pin3, LOW);
+}
+if (sensorValue<400 && belohnung == LOW){  // should be empty and it is
+digitalWrite(Pin0, LOW);
+      digitalWrite(Pin1, LOW);
+      digitalWrite(Pin2, LOW);
+      digitalWrite(Pin3, LOW);
+}
+  
+if (sensorValue<400 && belohnung == HIGH){  // should be full but it's not
+  
+    forward = true;
+  switch(step) {
+    case 0:
+      digitalWrite(Pin0, LOW);
+      digitalWrite(Pin1, LOW);
+      digitalWrite(Pin2, LOW);
+      digitalWrite(Pin3, HIGH);
+      break;
+    case 1:
+      digitalWrite(Pin0, LOW);
+      digitalWrite(Pin1, LOW);
+      digitalWrite(Pin2, HIGH);
+      digitalWrite(Pin3, HIGH);
+      break;
+    case 2:
+      digitalWrite(Pin0, LOW);
+      digitalWrite(Pin1, LOW);
+      digitalWrite(Pin2, HIGH);
+      digitalWrite(Pin3, LOW);
+      break;
+    case 3:
+      digitalWrite(Pin0, LOW);
+      digitalWrite(Pin1, HIGH);
+      digitalWrite(Pin2, HIGH);
+      digitalWrite(Pin3, LOW);
+      break;
+    case 4:
+      digitalWrite(Pin0, LOW);
+      digitalWrite(Pin1, HIGH);
+      digitalWrite(Pin2, LOW);
+      digitalWrite(Pin3, LOW);
+      break;
+    case 5:
+      digitalWrite(Pin0, HIGH);
+      digitalWrite(Pin1, HIGH);
+      digitalWrite(Pin2, LOW);
+      digitalWrite(Pin3, LOW);
+      break;
+    case 6:
+      digitalWrite(Pin0, HIGH);
+      digitalWrite(Pin1, LOW);
+      digitalWrite(Pin2, LOW);
+      digitalWrite(Pin3, LOW);
+      break;
+    case 7:
+      digitalWrite(Pin0, HIGH);
+      digitalWrite(Pin1, LOW);
+      digitalWrite(Pin2, LOW);
+      digitalWrite(Pin3, HIGH);
+      break;
+  }
+
+  if(forward) {
+    step++;
+  } else {
+    step--;
+  }
+  if(step>7) {
+    step=0;
+  }
+  if(step<0) {
+    step=7;
+  }
+  delay(1);
+}
+
+
+if (sensorValue2<400 && belohnung == LOW){  // should be halfway but it's below
+
+      forward = true;
+
+  switch(step) {
+    case 0:
+      digitalWrite(Pin0, LOW);
+      digitalWrite(Pin1, LOW);
+      digitalWrite(Pin2, LOW);
+      digitalWrite(Pin3, HIGH);
+      break;
+    case 1:
+      digitalWrite(Pin0, LOW);
+      digitalWrite(Pin1, LOW);
+      digitalWrite(Pin2, HIGH);
+      digitalWrite(Pin3, HIGH);
+      break;
+    case 2:
+      digitalWrite(Pin0, LOW);
+      digitalWrite(Pin1, LOW);
+      digitalWrite(Pin2, HIGH);
+      digitalWrite(Pin3, LOW);
+      break;
+    case 3:
+      digitalWrite(Pin0, LOW);
+      digitalWrite(Pin1, HIGH);
+      digitalWrite(Pin2, HIGH);
+      digitalWrite(Pin3, LOW);
+      break;
+    case 4:
+      digitalWrite(Pin0, LOW);
+      digitalWrite(Pin1, HIGH);
+      digitalWrite(Pin2, LOW);
+      digitalWrite(Pin3, LOW);
+      break;
+    case 5:
+      digitalWrite(Pin0, HIGH);
+      digitalWrite(Pin1, HIGH);
+      digitalWrite(Pin2, LOW);
+      digitalWrite(Pin3, LOW);
+      break;
+    case 6:
+      digitalWrite(Pin0, HIGH);
+      digitalWrite(Pin1, LOW);
+      digitalWrite(Pin2, LOW);
+      digitalWrite(Pin3, LOW);
+      break;
+    case 7:
+      digitalWrite(Pin0, HIGH);
+      digitalWrite(Pin1, LOW);
+      digitalWrite(Pin2, LOW);
+      digitalWrite(Pin3, HIGH);
+      break;
+  }
+
+  if(forward) {
+    step++;
+  } else {
+    step--;
+  }
+  if(step>7) {
+    step=0;
+  }
+  if(step<0) {
+    step=7;
+  }
+  delay(1);
+  }
+ 
+if (sensorValue>400 && belohnung == LOW){  // should be halfway but it's full
+
+      forward = false;
+
+  switch(step) {
+    case 0:
+      digitalWrite(Pin0, LOW);
+      digitalWrite(Pin1, LOW);
+      digitalWrite(Pin2, LOW);
+      digitalWrite(Pin3, HIGH);
+      break;
+    case 1:
+      digitalWrite(Pin0, LOW);
+      digitalWrite(Pin1, LOW);
+      digitalWrite(Pin2, HIGH);
+      digitalWrite(Pin3, HIGH);
+      break;
+    case 2:
+      digitalWrite(Pin0, LOW);
+      digitalWrite(Pin1, LOW);
+      digitalWrite(Pin2, HIGH);
+      digitalWrite(Pin3, LOW);
+      break;
+    case 3:
+      digitalWrite(Pin0, LOW);
+      digitalWrite(Pin1, HIGH);
+      digitalWrite(Pin2, HIGH);
+      digitalWrite(Pin3, LOW);
+      break;
+    case 4:
+      digitalWrite(Pin0, LOW);
+      digitalWrite(Pin1, HIGH);
+      digitalWrite(Pin2, LOW);
+      digitalWrite(Pin3, LOW);
+      break;
+    case 5:
+      digitalWrite(Pin0, HIGH);
+      digitalWrite(Pin1, HIGH);
+      digitalWrite(Pin2, LOW);
+      digitalWrite(Pin3, LOW);
+      break;
+    case 6:
+      digitalWrite(Pin0, HIGH);
+      digitalWrite(Pin1, LOW);
+      digitalWrite(Pin2, LOW);
+      digitalWrite(Pin3, LOW);
+      break;
+    case 7:
+      digitalWrite(Pin0, HIGH);
+      digitalWrite(Pin1, LOW);
+      digitalWrite(Pin2, LOW);
+      digitalWrite(Pin3, HIGH);
+      break;
+  }
+
+  if(forward) {
+    step++;
+  } else {
+    step--;
+  }
+  if(step>7) {
+    step=0;
+  }
+  if(step<0) {
+    step=7;
+  }
+  delay(1);
+  
+ }
+}
---
+++ codeMeasure.ino
@@ -0,0 +1,94 @@
+// Measuring part of the artificial flower.
+// by Benjamin H Paffhausen; MIT Licence
+// Measures the attendence of bees (copper plate under the feeding opening conected to the FDC), 
+// the successfull presentation of sucrosewater and the ON/OFF state of the rewarding LED.
+// Thopse values are saved to a table on the SD card.
+// The FDC1004 Lib is from https://github.com/beshaya/FDC1004
+
+#include <Wire.h>
+#include <FDC1004.h> 
+#include <SPI.h>
+#include <SD.h>
+FDC1004 fdc(FDC1004_100HZ);
+const int chipSelect = 10;
+unsigned long zeit = millis();
+unsigned long zeit2 = millis();
+
+void setup() {
+  Wire.begin();
+  SD.begin(chipSelect);
+
+  Serial.begin(250000);
+
+}
+
+void loop() {
+
+  String dataString = "";
+  
+//for (int kk=0;kk<25;kk++){
+int32_t  capacitance1 = fdc.getCapacitance(0);
+ dataString += String(capacitance1);
+   dataString += " ";
+   analogRead(A0);
+   analogRead(A0);
+ dataString += String(analogRead(A0)+analogRead(A0)+analogRead(A0)+analogRead(A0)+analogRead(A0)+analogRead(A0)+analogRead(A0)+analogRead(A0));
+   dataString += " ";
+   analogRead(A1);
+   analogRead(A1);
+    dataString += String(analogRead(A1));
+   dataString += " ";
+   
+//}
+
+
+
+//   dataString += " ";
+
+   
+   dataString += String(millis() - zeit);
+
+   File dataFile = SD.open("datalog.txt", FILE_WRITE);
+
+//  Serial.println(dataString);
+  zeit = millis();
+  // zeit2 = millis();
+//    Serial.print("  ");
+//  Serial.print(capacitance1);
+//    Serial.print("  ");
+//  Serial.print(capacitance2);
+//    Serial.print("  ");
+//  Serial.print(capacitance3);
+//    Serial.print("  ");
+//  Serial.print(capacitance4);
+//    Serial.print("  ");
+//  Serial.print(capacitance5);
+//    Serial.print("  ");
+//  Serial.print(capacitance6);
+//    Serial.print("  ");
+//  Serial.print(capacitance7);
+//    Serial.print("  ");
+//  Serial.print(capacitance8);
+//   Serial.print("  ");
+//   Serial.print(capacitance9);
+//     Serial.print("  ");
+//  Serial.println(capacitance0);
+
+   dataFile.println(dataString);
+
+   dataFile.close();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
